7abe0fe82e3cbe1a4c30076852714922cfdd13d3
Artemides <edmpulasky@gmail.com>
2023-08-01 16:57:04 -0500

    setup sepolia and etherscan keys

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,15 +2,30 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import "ethe";
 import "hardhat-deploy";
+
+const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "";
+const METAMASK_PK = process.env.METAMASK_PRIVATE_KEY || "";
+const ALCHEMY_SEPOLIA_RPC = process.env.ALCHEMY_SEPOLIA_RPC || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_SEPOLIA_RPC,
+      accounts: [METAMASK_PK],
+    },
+  },
   namedAccounts: {
     deployer: {
       default: 0,
       31338: 9,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN,
   },
 };
 
